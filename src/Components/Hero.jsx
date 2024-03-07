7fbef2b5d16ef4885165023f0fe3ee702caa53e0
Santosharawn7 <santosharawn7@gmail.com>
2024-03-06 20:35:00 -0500

    final

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,14 +5,14 @@ const Hero = () => {
   return (
 
 
-    <section class="relative md:py-36 font-serif overflow-hidden bg-gray-100">
-      <img class="xs:block absolute top-0 left-0 w-full h-full md:object-top object-cover" src="./Assets/work work.jpg" alt=""/>
+    <section class="relative md:py-36 font-serif overflow-hidden  bg-gray-700">
+      <img class="xs:block absolute top-0 left-0 w-full h-full bg-opacity-45  md:object-top object-cover" src="./Assets/building.png" alt=""/>
       <div class="relative container px-4 mx-auto">
         <div class=" pt-24">
-          <h1 class="font-heading text-4xl sm:text-6xl text-center  rounded-3xl p-4 text-yellow-900 lg:text-8xl xl:text-10xl font-bold mb-12">
+          <h1 class="font-heading text-4xl sm:text-6xl text-center  rounded-3xl p-4 text-white lg:text-8xl xl:text-10xl font-bold mb-12">
             <span>Royal Associates </span>
           </h1>
-          <h3 class="font-headingtext-3xl text-center rounded-3xl  text-yellow-500 lg:text-5xl xl:text-5xl font-bold mb-12">
+          <h3 class="font-headingtext-3xl text-center rounded-3xl  text-white lg:text-5xl xl:text-5xl font-semibold mb-12">
             <span>Legal Consulting </span>
           </h3>
           <div class="h-[300px] mx-auto w-fit">
